1584a396fde7d2a3ebad3950cd4054ba7ffe745a
Ahmed Aglan <pc@DESKTOP-9AIT25Q>
2023-10-09 16:51:31 +0300

    fix Registerion

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -8,26 +8,26 @@ using System.Threading.Tasks;
 
 namespace AribTask.Models
 {
-    //public class Employee : BaseEntity
-    //{
-    //    public string FirstName { get; set; }
-    //    public string LastName { get; set; }
-    //    public decimal Salary { get; set; }
-    //    public string ImagePath { get; set; }
+  //public class Employee : BaseEntity
+  //{
+  //    public string FirstName { get; set; }
+  //    public string LastName { get; set; }
+  //    public decimal Salary { get; set; }
+  //    public string ImagePath { get; set; }
 
-    //    //// Foreign key to represent the department the employee belongs to
-    //    //[ForeignKey("Department")]
-    //    //public int DepartmentId { get; set; }
-    //    //public virtual Department Department { get; set; }
+  //    //// Foreign key to represent the department the employee belongs to
+  //    //[ForeignKey("Department")]
+  //    //public int DepartmentId { get; set; }
+  //    //public virtual Department Department { get; set; }
 
-    //    // Foreign key to represent the manager of the employee
-    //    [ForeignKey("Manager")]
-    //    public int? ManagerId { get; set; }
-    //    public virtual Employee Manager { get; set; }
+  //    // Foreign key to represent the manager of the employee
+  //    [ForeignKey("Manager")]
+  //    public int? ManagerId { get; set; }
+  //    public virtual Employee Manager { get; set; }
 
-    //    public ICollection<EmployeeTask> EmployeeTasks { get; set; }
+  //    public ICollection<EmployeeTask> EmployeeTasks { get; set; }
 
-    //}
+  //}
   public class Employee : BaseEntity
   {
     [ForeignKey("User")]
@@ -39,9 +39,9 @@ namespace AribTask.Models
     public string LastName { get; set; }
     public decimal Salary { get; set; }
     public string ImageFileName { get; set; } // Add a new property for the unique file name
-		public string ImageFile { get; set; }
-		// Foreign key to represent the manager of the employee
-		public int? ManagerId { get; set; }
+    public string ImageFile { get; set; }
+    // Foreign key to represent the manager of the employee
+    public int? ManagerId { get; set; }
     public virtual Employee Manager { get; set; }
 
     // Navigation property to represent the tasks assigned to the employee
@@ -50,5 +50,9 @@ namespace AribTask.Models
     // Navigation property to represent the department the employee belongs to
     public int DepartmentId { get; set; }
     public virtual Department Department { get; set; }
+    public Employee(){
+
+      Department = new Department();
+}
   }
 }
